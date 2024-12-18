ed673d35ea72ebf5fd4c05fcd8f0a3d83252698d
ADE YOGA <85540115+yorisofficial@users.noreply.github.com>
2024-12-18 12:15:37 +0800

    Fix logic redirect page

--- src/app/(page)/auth/success/page.tsx
+++ src/app/(page)/auth/success/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import MessageRedirect from "@/app/components/MessageRedirect";
 
 const SuccessPage = () => {
-  const router = useRouter();
-  useEffect(() => {
-    const second = setTimeout(() => {
-      router.push("/auth");
-    }, 1000);
-
-    return () => {
-      clearTimeout(second);
-    };
-  }, [router]);
-
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      You have redirect to auth page
-    </div>
+    <>
+      <MessageRedirect />
+    </>
   );
 };
 
